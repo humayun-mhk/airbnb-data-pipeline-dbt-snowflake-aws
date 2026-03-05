df063fd38db3b2479e0809a7cb3645ef00a92d20
Humayun Khan <your-email@example.com>
2026-03-05 09:36:27 +0500

    Project done

---
+++ aws_dbt_snowflake_project/analyses/loops.sql
@@ -0,0 +1,7 @@
+{% set cols = ['BOOKING_ID', 'NIGHTS_BOOKED', 'BOOKING_AMOUNT'] %}
+select
+{% for col in cols %}
+   {{col}}
+    {% if not loop.last %},{% endif %}
+{% endfor %}
+from {{ref('bronze_bookings')}} 
